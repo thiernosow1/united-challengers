504626924d233e803bbd5190f74a3069f0a85c8c
Camille <camille.gcn@outlook.com>
2020-05-25 16:10:05 +0200

    version propre - début du projet

---
+++ client/build/precache-manifest.98f20943a5eefb3762fad6ec8748d132.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce096b6c16770259bd980e6a64386e13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0178dd460e6f504fc846",
+    "url": "/static/css/main.f3fc2ad7.chunk.css"
+  },
+  {
+    "revision": "b58f0f2a8780107702c3",
+    "url": "/static/js/2.26fb2fa1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.26fb2fa1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0178dd460e6f504fc846",
+    "url": "/static/js/main.2c269bf8.chunk.js"
+  },
+  {
+    "revision": "8383107cbc3fb07cdb54",
+    "url": "/static/js/runtime-main.92e3840b.js"
+  }
+]);
