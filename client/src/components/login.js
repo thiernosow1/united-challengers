9c720563d2537dcc71fca74f48c841c4c364b4d0
Camille <camille.gcn@outlook.com>
2020-05-27 11:57:00 +0200

    double redirection

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
+import {Link} from 'react-router-dom';
 import '../assets/css/app.css';
+import { render } from 'react-dom';
 
-function Login(){
+export default class Login extends Component{
+    render(){
     return (
-       <p> page du login </p>
+
+    <div>
+       <p> page du login ou on a le choix </p>
+           <Link to={'/LoginForm'} id="BTN">
+                 <p> Direction le formulaire</p>
+            </Link> 
+    </div>
+    );
+}
+}
+
+
+export class LoginForm extends Component{
+    render(){
+    return (
+        <div>
+       <p> page du login form </p>
+       </div>
     );
 
 }
+}
 
-export default Login; 
